4d7fafce7566d54696c162af3f7b86ab06c6b22b
p-galvan <p.galvant@gmail.com>
2024-11-12 18:59:00 -0800

    feat: expand TODO list

--- script.js
+++ script.js
@@ -4,6 +4,11 @@
 
 // TODO: add event listener for hover when interacting with boxes
 
+// TODO: add logic for:
+// - default number of squares (16 x 16)
+// - prompting user after pushing button for new grid size
+// - removing previous grid and creating new grid in same amount of space as default grid
+
 // Extra Credit:
 // Make squares darken by 10% with every interaction
 // Make every interaction randomly change the color of each square
