4b1c1af489a869cf0b9db3b3026aa21fcd9f4709
p-galvan <p.galvant@gmail.com>
2024-11-12 18:55:47 -0800

    feat: add TODO list

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-// Script for etch-a-sketch
+// TODO: create grid of squares with flexbox
+
+// TODO: create CSS class to make boxes square, create class for making squares black 
+
+// TODO: add event listener for hover when interacting with boxes
+
+// Extra Credit:
+// Make squares darken by 10% with every interaction
+// Make every interaction randomly change the color of each square
+// Maybe: toggle button to switch between modes?
