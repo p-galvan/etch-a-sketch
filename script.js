901580498b0776c1eb83972fa2b0cd96a3a14ac9
p-galvan <p.galvant@gmail.com>
2024-11-14 15:58:48 -0800

    feat: create functions to populate grid and    change color on hover
        Populates grid by creating divs spanning
        width of container in flex-direction column. Each
        of these divs holds the individiual square divs
        for that row. Colors each square by using
        mouseover event.

--- script.js
+++ script.js
@@ -1,21 +1,43 @@
-// TODO: create grid of squares with flexbox
-const INITIAL_SIZE = 16;
+const INITIAL_WIDTH = 16;
 
-function createInitialGrid(size) {
+function populateGrid(size) {
     const container = document.querySelector("#etch-container");
-    const square; 
-
+    let square = null;
+    let row = null;
+    
+    // Create rows to hold individual squares
     for (let i = 0; i < size; i++) {
-        square = document.createElement("div");
+        
+        row = document.createElement("div");
+        row.setAttribute("class", "flex-row");
 
-        // Add attributes?
-        square.setAttribute("class", "gridsquare");
-        // Append to squares
-        container.appendChild(square);
+        // Create individual squares
+        for (let j = 0; j < size; j++) {
+            square = document.createElement("div");
+            square.setAttribute("class", "flex-square");
+
+            // Append each square to row
+            row.appendChild(square);
+        }
+        
+        // Append each row to container
+        container.appendChild(row);
     }
 }
 
-createInitialGrid(INITIAL_SIZE);
+function onHover() {
+    let rows = document.querySelectorAll(".flex-row");
+    
+    // Use mouseover event to trigger when moving inside child of each row
+    rows.forEach(row => {
+        row.addEventListener("mouseover", event => {
+            event.target.className = "flex-square square-pressed";
+        });
+    });
+}
+
+populateGrid(INITIAL_WIDTH);
+onHover();
 
 // TODO: create CSS class to make boxes square, create class for making squares black 
 
@@ -25,7 +47,6 @@ createInitialGrid(INITIAL_SIZE);
 // - default number of squares (16 x 16)
 // - prompting user after pushing button for new grid size
 // - removing previous grid and creating new grid in same amount of space as default grid
-
 // Extra Credit:
 // Make squares darken by 10% with every interaction
 // Make every interaction randomly change the color of each square
